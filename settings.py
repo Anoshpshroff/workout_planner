f2eb52f5eca65e552cab4d7d1f399046b5161b74
Anoshpshroff <117518122+Anoshpshroff@users.noreply.github.com>
2025-08-13 20:45:57 +0530

    Update settings.py

--- settings.py
+++ settings.py
@@ -16,7 +16,7 @@ from pathlib import Path
 load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-GOOGLE_AI_API_KEY = os.getenv('GOOGLE_AI_API_KEY', 'AIzaSyAdzMaPpr_fwV6dgCQCzjPcb6Z2jfeJCdg')
+GOOGLE_AI_API_KEY = os.getenv('GOOGLE_AI_API_KEY', '')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
